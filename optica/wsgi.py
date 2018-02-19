152e3b612355b854578b65432b994d5652f57057
kevinmike94 <kevin_22894@hotmail.com>
2018-02-18 22:32:04 -0500

    Adding information for deployment

--- optica/wsgi.py
+++ optica/wsgi.py
@@ -7,28 +7,29 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
-import os
+# Development configuration
 
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "optica.settings")
-
-application = get_wsgi_application()
-
-
-
-# print "hola joven"0
 # import os
-# import sys
-# import site
 #
-# # Add the site-packages of the chosen virtualenv to work with
-# site.addsitedir('C:/Users/usurio/optica-latyna/Lib/site-packages')
-# # Add the app's directory to the PYTHONPATH
-# sys.path.append('c:/xampp/htdocs/optica')
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'optica.settings'
-# # Activate your virtual env
-# activate_env=os.path.expanduser("C:/Users/usurio/optica-latyna/Scripts/activate_this.py")
-# execfile(activate_env, dict(__file__=activate_env))
-#from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
+# from django.core.wsgi import get_wsgi_application
+#
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "optica.settings")
+#
+# application = get_wsgi_application()
+
+# Production configuration
+
+import os
+import sys
+import site
+
+# Add the site-packages of the chosen virtualenv to work with
+site.addsitedir('D:/Proyectos/Django/optica/opticaenv/Lib/site-packages')
+# Add the app's directory to the PYTHONPATH
+sys.path.append('D:/Proyectos/Django/optica')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'optica.settings'
+# Activate your virtual env
+activate_env=os.path.expanduser("D:/Proyectos/Django/optica/opticaenv/Scripts/activate_this.py")
+execfile(activate_env, dict(__file__=activate_env))
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
